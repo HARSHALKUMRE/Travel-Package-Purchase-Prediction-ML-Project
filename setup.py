5116f373d052df6a84318ef268ea0da95fc7bb62
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-03-03 14:06:28 +0530

    setup file and requirements added.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="travel_pack",
+    version="0.0.1",
+    author="Harshal Kumre",
+    author_email="kumreharshalkumar@gmail.com",
+    packages=find_packages()
+)
